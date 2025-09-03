06c817debae17fc605d42765b56323784afeefb2
Kimheekyo <khkyo010305@gmail.com>
2025-09-03 09:43:06 +0900

    Title: 배열 채우기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250903/배열 채우기/filling-array.py
+++ 250903/배열 채우기/filling-array.py
@@ -1,11 +1,15 @@
 arr=list(map(int,input().split()))
 
-new_arr=[i for i in arr[::-1]]
 
-for i,k in enumerate(new_arr):
+new_arr=[]
+for i,k in enumerate(arr):
     if i==0 and k==0:
         pass
     elif k==0:
         break
     else:
-        print(k,end=' ')
+        new_arr.append(k)
+
+for i in new_arr[::-1]:
+    print(i,end=" ")
+
