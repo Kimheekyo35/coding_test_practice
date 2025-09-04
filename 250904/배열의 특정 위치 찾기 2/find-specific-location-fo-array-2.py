7434ca6b1aab1c7bb0c49d485e8cf80126c3990f
Kimheekyo <khkyo010305@gmail.com>
2025-09-04 22:11:52 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250904/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
@@ -0,0 +1,14 @@
+arr=list(map(int,input().split()))
+
+sum_even=0
+sum_odd=0
+for i in range(len(arr)):
+    if i%2==0:
+        sum_even+=arr[i]
+    else:
+        sum_odd+=arr[i]
+
+if sum_even>sum_odd:
+    print(sum_even-sum_odd)
+else:
+    print(sum_odd-sum_even)
