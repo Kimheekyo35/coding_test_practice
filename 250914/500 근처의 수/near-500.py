41d1db142fa711be3a06f3a8c857c974d771d4e1
Kimheekyo <khkyo010305@gmail.com>
2025-09-14 01:49:23 +0900

    Title: 500 근처의 수, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 250914/500 근처의 수/near-500.py
+++ 250914/500 근처의 수/near-500.py
@@ -5,6 +5,17 @@ under_500_list=[i for i in arr if i<500]
 over_500_list=[i for i in arr if i>500]
 
 
+max_val=under_500_list[0]
+for i in under_500_list:
+    if max_val<i:
+        max_val=i
 
-print(max(under_500_list), min(over_500_list))
+min_val=over_500_list[0]
+for i in over_500_list:
+    if min_val>i:
+        min_val=i
+
+
+print(max_val, min_val)
+# print(max(under_500_list), min(over_500_list))
     
