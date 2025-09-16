af0af333c38f989d6ab111a5e454c6f27fbbfa13
Kimheekyo <khkyo010305@gmail.com>
2025-09-16 23:17:05 +0900

    Title: 두 수의 차의 최솟값, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250916/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 250916/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -4,10 +4,18 @@ n=int(input())
 arr=list(map(int,input().split()))
 
 min_val=99
-for i in range(n):
-    for j in range(i+1,n):
-        if min_val>arr[j]-arr[i]:
-            min_val=arr[j]-arr[i]
+# for i in range(n):
+#     for j in range(i+1,n):
+#         if min_val>arr[j]-arr[i]:
+#             min_val=arr[j]-arr[i]
 
+# print(min_val)
+
+for i in range(n-1):
+    if min_val>arr[i+1]-arr[i]:
+        min_val=arr[i+1]-arr[i]
 print(min_val)
-        
+
+
+# 오름차순이라는 말 중요!!
+# 오름차순이므로 인접한 두 값의 차가 가장 작음.
