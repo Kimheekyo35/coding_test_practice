fe77c76b0f2ae799d4cb93a0d715f0e552bf438a
Kimheekyo <khkyo010305@gmail.com>
2025-09-26 10:17:02 +0900

    Title: 배열 놀이, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250926/배열 놀이/play-with-array.py
@@ -0,0 +1,24 @@
+n,q=tuple(map(int,input().split()))
+
+arr=list(map(int,input().split()))
+
+
+for _ in range(q):
+    ques_list=tuple(map(int,input().split()))
+
+    
+
+    if ques_list[0]==1:
+        index_num=ques_list[1]
+        print(arr[index_num-1])
+    elif ques_list[0]==2:
+        idx=-1
+        if ques_list[1] in arr:
+            answer=arr.index(ques_list[1])
+        print(answer+1)
+
+    else:
+        for elem in range(ques_list[1]-1,ques_list[2]):
+            print(arr[elem],end=' ')
+        print()
+            
