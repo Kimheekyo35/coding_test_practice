276492fa0a155a31c78d82161c84a08415949a74
Kimheekyo <khkyo010305@gmail.com>
2025-10-03 23:13:53 +0900

    Title: 공백을 기준으로 출력, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251003/공백을 기준으로 출력/output-based-on-space.py
+++ 251003/공백을 기준으로 출력/output-based-on-space.py
@@ -1,12 +1,4 @@
-string1 = input()
-string2 = input()
+str1=input()
+str1+=input()
 
-for str1 in string1:
-    if ' ' in str1:
-        continue
-    print(str1,end='')
-
-for str2 in string2:
-    if ' ' in str2:
-        continue
-    print(str2, end='')
+print(str1.replace(" ",""))
