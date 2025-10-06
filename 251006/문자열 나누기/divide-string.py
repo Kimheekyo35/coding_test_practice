48992f7505b269ca038e4f87d3de222ba01b2293
Kimheekyo <khkyo010305@gmail.com>
2025-10-06 22:59:50 +0900

    Title: 문자열 나누기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251006/문자열 나누기/divide-string.py
+++ 251006/문자열 나누기/divide-string.py
@@ -3,11 +3,11 @@ num_list = list(map(str,input().split()))
 
 num_str = "".join(num_list)
 new = ""
-cnt = 0
+
 for i in range(len(num_str)):
     new += num_str[i]
     if (i+1)%5 == 0:
         print(new)
         new=""
-        cnt +=1
-print(cnt)
+
+print(new)
