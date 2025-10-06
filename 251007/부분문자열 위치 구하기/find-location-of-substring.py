d2877e7a2a02bd04b446a66cb701eb395577004b
Kimheekyo <khkyo010305@gmail.com>
2025-10-07 00:02:00 +0900

    Title: 부분문자열 위치 구하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251007/부분문자열 위치 구하기/find-location-of-substring.py
@@ -0,0 +1,14 @@
+input_str = input()
+target_str = input()
+
+idx = 0
+# Please write your code here.
+for i in range(len(input_str)-len(target_str)+1):
+    if input_str[i:i+len(target_str)] == target_str :
+        idx = input_str.index(target_str[0])
+
+if idx:
+    print(idx)
+else:
+    print("-1")
+
