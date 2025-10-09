4addd5d604e9beaa159686a8ff9254a24d53fa80
Kimheekyo <khkyo010305@gmail.com>
2025-10-10 00:10:09 +0900

    Title: 문자열 놀이, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251010/문자열 놀이/play-with-string.py
+++ 251010/문자열 놀이/play-with-string.py
@@ -1,33 +1,33 @@
 str1, q_num = input().split()
-print(str[2])
 q_num = int(q_num)
 
 
-# for _ in range(q_num):
-#     new_str=''
-#     num,q1,q2 = input().split()
-#     num = int(num)
-#     if num == 1:
-#         q1 = int(q1)
-#         q2 = int(q2)
-#         a = str1[q1-1]
-#         b = str1[q2-1]
-#         for i in range (len(str1)):
-#             if i == q1-1:
-#                 new_str = str1[:i] + b + str1[i+1:]
-#             elif i == q2-1:
-#                 new_str = str1[:i] + a + str1[i+1:]
+for _ in range(q_num):
+    num,q1,q2 = input().split()
+    num = int(num)
+    if num == 1:
+        q1 = int(q1)
+        q2 = int(q2)
+        a = str1[q1-1]
+        b = str1[q2-1]
 
-#         print(new_str)
 
-#     elif num == 2:
-#         str1 = list(str1)
-#         new =[]
-#         for elem in new:
-#             if elem == q1:
-#                 elem = q2
-#                 new.append(elem)
-#             else:
-#                 new.append(elem)
-#         new = "".join(new)
-#         print(new)
+        for i in range(len(str1)):
+            if i == q1-1:
+                str1 = str1[:i] + b + str1[i+1:]
+            elif i == q2-1:
+                str1 = str1[:i] + a + str1[i+1:]
+
+        print(str1)
+
+    elif num == 2:
+        str1 = list(str1)
+        new =[]
+        for elem in str1:
+            if elem == q1:
+                elem = q2
+                new.append(elem)
+            else:
+                new.append(elem)
+        str1 = "".join(new)
+        print(str1)
