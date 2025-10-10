5596947c7864e7876932c0830f21bce1b422490f
Kimheekyo <khkyo010305@gmail.com>
2025-10-10 10:27:11 +0900

    Title: 문자 제거하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251010/문자 제거하기/del-char.py
+++ 251010/문자 제거하기/del-char.py
@@ -1,27 +1,28 @@
 string = input()
 for _ in range(len(string)-1):
-    
+    list_str = list(string)
     pop_idx = int(input())
 
-    if pop_idx >= len(string):
-        string = string[:-1]
-        print(string)
-    elif len(string) == 1:
+#     if pop_idx >= len(string):
+#         string = string[:-1]
+
+#     elif len(string) == 1:
+#         break
+#     else:
+#         string = string[:pop_idx] + string[pop_idx+1:]
+
+#     print(string)
+    if len(list_str) == 1:
         break
+    elif pop_idx >= len(string):
+        list_str.pop(-1)
+        
     else:
-        string = string[:pop_idx] + string[pop_idx+1:]
-        print(string)
+        list_str.pop(pop_idx)
+        new_string = "".join
     
-    
-    # if len(list_str) == 1:
-    #     break
-    # elif pop_idx >= len(string):
-    #     list_str.pop(-1)
-    #     print(list_str)
-    # else:
-    #     list_str.pop(pop_idx)
-    #     new_string = "".join
-    #     print(list_str)
+    arr = "".join(list_str)
+    print(arr)
 
 
 
