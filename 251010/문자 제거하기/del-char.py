6e8c2a4f2fc28ad51e08f8ca2f9a9f03cb066974
Kimheekyo <khkyo010305@gmail.com>
2025-10-10 10:33:12 +0900

    Title: 문자 제거하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251010/문자 제거하기/del-char.py
+++ 251010/문자 제거하기/del-char.py
@@ -2,7 +2,6 @@ string = input()
 # for문 돌 때마다 갱신되므로 for문 밖에다가 정의해야됨
 list_str = list(string)
 for _ in range(len(string)-1):
-    
     pop_idx = int(input())
 
 #     if pop_idx >= len(string):
@@ -16,7 +15,7 @@ for _ in range(len(string)-1):
 #     print(string)
     if len(list_str) == 1:
         break
-    elif pop_idx >= len(string):
+    elif pop_idx >= len(list_str):
         list_str.pop(-1)
         
     else:
