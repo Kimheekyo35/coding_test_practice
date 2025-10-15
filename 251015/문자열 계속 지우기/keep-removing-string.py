38815dcf9a36bc2a3d66bf13c14fac5618ca90ea
Kimheekyo <khkyo010305@gmail.com>
2025-10-15 23:33:58 +0900

    Title: 문자열 계속 지우기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251015/문자열 계속 지우기/keep-removing-string.py
+++ 251015/문자열 계속 지우기/keep-removing-string.py
@@ -1,36 +1,38 @@
 A = input()
 B = input()
 
-len_a = len(A)
-len_b = len(B)
+# len_a = len(A)
+# len_b = len(B)
 
-while True:
-    idx = -1
+# while True:
+#     idx = -1
 
-    candidates = len_a - len_b + 1
+#     candidates = len_a - len_b + 1
 
-    for i in range(candidates):
-        is_same = True
-        for j in range(len_b):
-            if A[i+j] != B[j]:
-                is_same = False
-                break
-        if is_same:
-            idx = i
-            # 이 for문에서 탈출
-            break
+#     for i in range(candidates):
+#         is_same = True
+#         for j in range(len_b):
+#             if A[i+j] != B[j]:
+#                 is_same = False
+#                 break
+#         if is_same:
+#             idx = i
+#             # 이 for문에서 탈출
+#             break
     
-    if idx == -1:
-        break
+#     if idx == -1:
+#         break
 
-    # B의 길이가 유동적일 수도 있으니까 idx+2 로 고정하면 안됨.
-    A = A[:idx] + A[idx + len_b :]
+#     # B의 길이가 유동적일 수도 있으니까 idx+2 로 고정하면 안됨.
+#     A = A[:idx] + A[idx + len_b :]
 
-    len_a = len(A)
+#     len_a = len(A)
+
+# print(A)
+
+while B in A:
+    f = A.find(B)
+    A = A[:f] + A[f+len(B):]
 
 print(A)
-
-
-
-
         
