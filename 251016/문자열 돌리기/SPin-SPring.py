8746b8ab4bb6bded3aad20bcc404a29d0a2be76b
Kimheekyo <khkyo010305@gmail.com>
2025-10-16 23:04:32 +0900

    Title: 문자열 돌리기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251016/문자열 돌리기/SPin-SPring.py
+++ 251016/문자열 돌리기/SPin-SPring.py
@@ -1,10 +1,9 @@
 string = input()
 len_string = len(string)
 
-for i in range(len_string+1):
-    
-    if i%5 == 0:
-        print(string)
-    else:
-        string = string[-i] + string[i:]
+print(string)
+
+for _ in range(len_string):
+    string = string[-1] + string[:-1]
+
     print(string)
