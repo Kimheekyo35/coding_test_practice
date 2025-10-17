63bd43ac639a6fa6b2a55eaad330e37900aa284c
Kimheekyo <khkyo010305@gmail.com>
2025-10-17 21:06:54 +0900

    Title: 대문자 소문자 바꾸기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251017/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
+++ 251017/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
@@ -1,3 +1,8 @@
-print(ord('a'))
+string = input()
 
-print(ord('A'))
+for s in string:
+    if s.isalpha():
+        if ord(s) >= 65 and ord(s) <= 96:
+            print(s.lower() , end = "")
+        elif ord(s) >= 97:
+            print(s.upper() , end = "")
