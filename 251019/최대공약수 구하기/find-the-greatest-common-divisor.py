727f3dd7f59daa47ca8b2ab4042b8834b8da0211
Kimheekyo <khkyo010305@gmail.com>
2025-10-19 16:21:55 +0900

    Title: 최대공약수 구하기, Time: 77ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251019/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def comp_num(n):
+    num_list = [ i for i in range(1,n) if n % i == 0]
+    return num_list
+
+def gcd(n,m):
+    n_list = comp_num(n)
+    m_list = comp_num(m)
+    gcd_list = []
+    for i in n_list:
+        for j in m_list:
+            if i == j : gcd_list.append(i)
+    return max(gcd_list)
+
+print(gcd(n,m))
