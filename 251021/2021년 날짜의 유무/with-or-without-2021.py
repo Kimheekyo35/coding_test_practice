0d9694aa99f45b87d56bad3b5c9d6a59018b865f
Kimheekyo <khkyo010305@gmail.com>
2025-10-21 15:53:09 +0900

    Title: 2021년 날짜의 유무, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251021/2021년 날짜의 유무/with-or-without-2021.py
+++ 251021/2021년 날짜의 유무/with-or-without-2021.py
@@ -9,24 +9,19 @@ def day(D,max_d):
     return False
 
 def month_check(M,D):
-    if M == 1 or M == 3 or M == 5 or M == 7 or M == 8 or M == 10 or M == 12:
+    if M in [1, 3, 5, 7, 8, 10, 12]:
         max_d = 31
-        if day(D,max_d):
-            return True
+
     elif M == 2:
         max_d = 28
-        if day(D,max_d):
-            return True
     
-    elif M == 4 or M == 6 or M == 9 or M == 11:
+    elif M in [4,6,9,11]:
         max_d = 30
-        if day(D,max_d):
-            return True
+
     else:
         return False
 
-
-    return False
+    return 1 <= D <= max_d
 
 if month_check(M,D):
     print("Yes")
