b7603509feee9234775a61a07d249bc800c920c8
Kimheekyo <khkyo010305@gmail.com>
2025-10-22 09:41:53 +0900

    Title: 그 계절, 그 날, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251022/그 계절, 그 날/that-season-that-day.py
+++ 251022/그 계절, 그 날/that-season-that-day.py
@@ -16,14 +16,11 @@ def season(M):
 def day(Y):
     # 윤년이면 True
     if (Y % 4 == 0) :
-        return True
-    elif (Y % 4 == 0) or (Y % 100 == 0):
-        return True
-    elif (Y % 4 == 0 and Y % 100 == 0) and (Y % 400 == 0):
-        return True
-    else:
-        return False
+        if (Y % 100 != 0) or ((Y % 100 == 0) and (Y % 400 == 0)):
+            return True
+    return False
 
+    
 def check_day(Y,M,D):
     if M <= 12 and D <= 31:
         if M == 2:
