5b2e8a86bd2eb254de91a08e29af68b45154f2b2
Kimheekyo <khkyo010305@gmail.com>
2025-10-23 10:58:05 +0900

    Title: 특정 구간의 합, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251023/특정 구간의 합/sum-of-specific-intervals.py
@@ -0,0 +1,22 @@
+n, m = map(int,input().split())
+arr = list(map(int,input().split()))
+queries = [tuple(map(int,input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+def cal_q(k):
+    a, b = k[0],k[1]
+    return a-1,b
+
+def sum_val():
+    for q in queries:
+        val = 0
+        a1, a2 = cal_q(q)
+        val = arr[a1:a2]
+        print(sum(val))
+
+sum_val()
+
+        
+
+        
