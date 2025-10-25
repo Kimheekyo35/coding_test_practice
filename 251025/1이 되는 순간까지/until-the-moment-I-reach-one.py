84e069f43aeb9b4c9cb5b785e5de58fe6972a15c
Kimheekyo <khkyo010305@gmail.com>
2025-10-25 20:42:24 +0900

    Title: 1이 되는 순간까지, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251025/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+# Please write your code here.
+cnt = 0
+
+def div(n):
+    global cnt
+    if n == 1:
+        return cnt
+    if n % 2 == 0:
+        cnt += 1
+        n //= 2
+        return div(n)
+    else:
+        cnt += 1
+        n //= 3
+        return div(n)
+
+print(div(N))
