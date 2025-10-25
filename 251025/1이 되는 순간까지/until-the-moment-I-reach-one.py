e3822b057c17c897971afdce197e18110273e2d0
Kimheekyo <khkyo010305@gmail.com>
2025-10-25 20:44:28 +0900

    Title: 1이 되는 순간까지, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 251025/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 251025/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -23,8 +23,8 @@ def get_num(n):
         return 0
     
     if n % 2 == 0:
-        return get_num(n) + 1
+        return get_num(n//2) + 1
     else:
-        return get_num(n) + 1
+        return get_num(n//3) + 1
 
 print(get_num(N))
