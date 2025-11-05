a93e448c1caae14b7ae147f3b9132982553463aa
Kimheekyo <khkyo010305@gmail.com>
2025-11-05 11:09:30 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251105/Date to Date/date-to-date.py
+++ 251105/Date to Date/date-to-date.py
@@ -5,11 +5,9 @@ m1, d1, m2, d2 = map(int, input().split())
 year_list = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 day_sum = 0
-for i in range(m1,m2):
-    if i == m1:
-        day_sum += (year_list[i] - d1) + 1
-    else:
-        day_sum += year_list[i]
-day_sum += d2
+for i in range(m1,m2+1):
+    day_sum += year_list[i]
+
+day_sum -= ((d1-1) + year_list[m2] - d2)
 
 print(day_sum)
