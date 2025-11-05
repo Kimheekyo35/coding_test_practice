bc96cc81d5ff80bdb53eed1366d1196ad2568917
Kimheekyo <khkyo010305@gmail.com>
2025-11-05 10:03:18 +0900

    Title: Time to Time, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251105/Time to Time/time-to-time.py
+++ 251105/Time to Time/time-to-time.py
@@ -1,5 +1,23 @@
-a, b, c, d = map(int, input().split())
+# a, b, c, d = map(int, input().split())
 
-# Please write your code here.
+# # Please write your code here.
 
-print(((60*c)+d)-((60*a)+b))
+# print(((60*c)+d)-((60*a)+b))
+
+a,b,c,d = map(int,input().split())
+
+hours, mins = a,b
+elapsed_time = 0
+
+while True:
+    if hours== c and mins == d:
+        break
+    
+    elapsed_time += 1
+    mins += 1
+    
+    if mins == 60:
+        hours += 1
+        mins = 0
+    
+print(elapsed_time)
