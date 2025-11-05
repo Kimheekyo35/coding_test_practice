ffd43329d7d5ad1823558c78d2786cd552f32d26
Kimheekyo <khkyo010305@gmail.com>
2025-11-05 13:06:45 +0900

    Title: DateTime to DateTime, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251105/DateTime to DateTime/datetime-to-datetime.py
+++ 251105/DateTime to DateTime/datetime-to-datetime.py
@@ -2,29 +2,28 @@ a, b, c = map(int, input().split())
 
 # Please write your code here.
 # min 이 60 넘어가면 hours += 1, hours가 24 넘어가면 day += 1
+day = 11
+time = 11
+fix_min = 11
 
+def calculate_day(a,b,c):
+    return ((a)*24*60+(60*b)+c)
+# def calculate(a,b,c):
+#     time_count = 0
 
-
-def calculate(a,b,c):
-    day = 11
-    time = 11
-    fix_min = 11
-    time_count = 0
-
-    if day == 11 and (time > b or fix_min > c):
-        return -1
-    
-    while True:
-        if day == a and time == b and fix_min == c:
-            return time_count
-            break
-        time_count += 1
-        fix_min += 1
-        if fix_min == 60:
-            time += 1
-            fix_min = 0
+#     while True:
+#         if day == a and time == b and fix_min == c:
+#             return time_count
+#             break
+#         time_count += 1
+#         fix_min += 1
+#         if fix_min == 60:
+#             time += 1
+#             fix_min = 0
         
-        if time == 24:
-            day += 1
-            time = 0
-print(calculate(a,b,c))
+#         if time == 24:
+#             day += 1
+#             time = 0
+# print(calculate(a,b,c))
+
+print(calculate_day(a,b,c)-calculate_day(11,11,11))
