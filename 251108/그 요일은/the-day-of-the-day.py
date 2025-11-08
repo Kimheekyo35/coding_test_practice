ad8c1e321d028fed7e37a5b7de63aa1e0ee575b2
Kimheekyo <khkyo010305@gmail.com>
2025-11-08 22:52:59 +0900

    Title: 그 요일은, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251108/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,29 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Please write your code here.
+
+month_day_list = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+def calculate_day(month,day):
+    total_day = 0
+    
+    for i in range(1,month):
+        total_day += month_day_list[i]
+    total_day += day
+
+    return total_day
+
+day_list = ['Mon','Tue','Web','Thu','Fri','Sat','Sun']
+
+if A in day_list:
+    index = day_list.index(A)
+
+diff = calculate_day(m2,d2) - calculate_day(m1,d1)
+count = 0
+while True:
+    if diff < index:
+        break
+    else:
+        diff -= 7
+    count += 1
+print(count)
