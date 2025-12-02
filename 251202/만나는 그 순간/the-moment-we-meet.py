2a1f5edd06daee2f2befc903dcd4fe0053209378
Kimheekyo <khkyo010305@gmail.com>
2025-12-02 21:08:04 +0900

    Title: 만나는 그 순간, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251202/만나는 그 순간/the-moment-we-meet.py
+++ 251202/만나는 그 순간/the-moment-we-meet.py
@@ -5,13 +5,13 @@ offset = 1000
 max_r = 2*offset
 
 list_a, list_b = [0]*(max_r+1), [0]*(max_r+1)
-cur = 0
 # 첫 번째
 for _ in range(n):
     d, t = tuple(input().split())
     for _ in range(int(t)):
         list_a[time_a] = list_a[time_a-1] + (1 if d == "R" else -1) 
         time_a += 1
+
 
 for _ in range(m):
     d, t = tuple(input().split())
@@ -20,7 +20,7 @@ for _ in range(m):
         time_b += 1
 
 ans = -1
-for i in range(1,time_a+1):
+for i in range(1,time_a):
     if list_a[i] == list_b[i]:
         ans = i
         break
