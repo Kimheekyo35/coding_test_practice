44cbe4db3d9f0ae33d0dbb8c6598edbd1da02a40
Kimheekyo <khkyo010305@gmail.com>
2025-12-06 22:52:57 +0900

    Title: 벌금은 누구에게, Time: 86ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251206/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,21 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+stud_cnt = [0]*N
+
+for s in student:
+    for idx,cnt in enumerate(stud_cnt):
+        if s == idx+1:
+            stud_cnt[idx] += 1
+
+answer = -1
+for stud in stud_cnt:
+    if stud == 3:
+        answer = stud_cnt.index(stud)+1
+        break
+
+print(answer)
+
+
+
+# Please write your code here.
