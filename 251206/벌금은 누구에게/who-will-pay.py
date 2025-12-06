713bf41d3ac3dc4991ab669c8af0e68828be7943
Kimheekyo <khkyo010305@gmail.com>
2025-12-06 23:16:42 +0900

    Title: 벌금은 누구에게, Time: 90ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 251206/벌금은 누구에게/who-will-pay.py
+++ 251206/벌금은 누구에게/who-will-pay.py
@@ -6,12 +6,14 @@ stud_cnt = [0]*N
 stud_rank = [0]*N
 answer = -1
 
+score = 1
 for s in student:
     for idx,cnt in enumerate(stud_cnt):
         if s == idx+1:
             stud_cnt[idx] += 1
         if cnt == K:
-            stud_rank[idx] += 1
+            stud_rank[idx] += score
+            score += 1
 
 for rank in stud_rank:
     if rank == 1:
