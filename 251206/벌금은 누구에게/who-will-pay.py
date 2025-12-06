3485ef1c63c029415b3c63b3ee2b22c90ee4de42
Kimheekyo <khkyo010305@gmail.com>
2025-12-06 23:15:45 +0900

    Title: 벌금은 누구에게, Time: 85ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 251206/벌금은 누구에게/who-will-pay.py
+++ 251206/벌금은 누구에게/who-will-pay.py
@@ -1,23 +1,22 @@
+import sys
 N, M, K = map(int, input().split())
 student = [int(input()) for _ in range(M)]
 
 stud_cnt = [0]*N
+stud_rank = [0]*N
+answer = -1
 
 for s in student:
     for idx,cnt in enumerate(stud_cnt):
         if s == idx+1:
             stud_cnt[idx] += 1
         if cnt == K:
-            break
+            stud_rank[idx] += 1
 
-answer = -1
-for stud in stud_cnt:
-    if stud == K:
-        answer = stud_cnt.index(stud)+1
+for rank in stud_rank:
+    if rank == 1:
+        answer = stud_rank.index(rank)+1
         break
 
 print(answer)
 
-
-
-# Please write your code here.
