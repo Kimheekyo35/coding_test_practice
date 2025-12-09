a1b794430e0834fa6a9c8b07958430c2936ef436
Kimheekyo <khkyo010305@gmail.com>
2025-12-09 22:59:29 +0900

    Title: 벌금은 누구에게, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251209/벌금은 누구에게/who-will-pay.py
+++ 251209/벌금은 누구에게/who-will-pay.py
@@ -2,12 +2,14 @@ n, m, k = map(int, input().split())
 lit = set()
 ans, cnt = -1, 0
 
+## 이 코드가 틀린 이유는 맨 처음에 들어온 것들이 cnt 0으로 찍히기 때문
 for _ in range(m):
     number = int(input())
     if number in lit:
         cnt += 1
     else:
         lit.add(number)
+        cnt += 1
     if cnt >= k:
         ans = number
         break
