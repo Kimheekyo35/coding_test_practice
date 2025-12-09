7c5f2ee53cc89f29e814e4eb17f9aec3bee7a498
Kimheekyo <khkyo010305@gmail.com>
2025-12-09 22:56:52 +0900

    Title: 벌금은 누구에게, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251209/벌금은 누구에게/who-will-pay.py
+++ 251209/벌금은 누구에게/who-will-pay.py
@@ -1,13 +1,14 @@
-n, m, k = tuple(map(int,input().split()))
-lit = []
+n, m, k = map(int, input().split())
+lit = set()
 ans, cnt = -1, 0
+
 for _ in range(m):
     number = int(input())
     if number in lit:
         cnt += 1
     else:
-        lit.append(number)
-    if cnt == k:
+        lit.add(number)
+    if cnt >= k:
         ans = number
         break
 
