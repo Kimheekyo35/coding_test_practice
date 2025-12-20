7a32300b2ffa85b7328a427a7478318e969ca809
Kimheekyo <khkyo010305@gmail.com>
2025-12-21 01:29:41 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251221/DateTime to DateTime/datetime-to-datetime.py
+++ 251221/DateTime to DateTime/datetime-to-datetime.py
@@ -3,6 +3,7 @@ a, b, c = tuple(map(int,input().split()))
 start = 11 + 11*60 + 60*24*10
 end = 24*60*(a-1) + b*60 + c
 
+# 예외를 if에 놓고 나머지는 else로 빼는 게 낫...
 if start<=end:
     print(end-start)
 else:
