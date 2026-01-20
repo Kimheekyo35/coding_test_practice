53d3d1b3038269a4dab97470f950b3b4091e7e34
Kimheekyo <khkyo010305@gmail.com>
2026-01-20 22:16:51 +0900

    Title: 1이 3개 이상 있는 위치, Time: 86ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 260120/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,29 @@
+n = int(input())
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+dxs = [0,1,0,-1]
+dys = [1,0,-1,0]
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+# 인접한 거 세주는 함수
+def adjacent_cnt(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dxs[dx], y+dys[dy]
+        if in_range(nx,ny) and arr[nx][ny]==1:
+            cnt += 1
+    return cnt
+
+# 각 칸 탐색
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if adjacent_cnt(i,j) >= 3:
+            ans += 1
+
+print(ans)
