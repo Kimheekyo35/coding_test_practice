64b533278442440d22b46aeb84547e8be5cff8e7
Kimheekyo <khkyo010305@gmail.com>
2026-02-08 21:22:29 +0900

    Title: 체크판위에서 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260208/체크판위에서 2/on-the-checkboard-2.py
+++ 260208/체크판위에서 2/on-the-checkboard-2.py
@@ -3,15 +3,21 @@ grid = [list(input().split()) for _ in range(R)]
 
 # Please write your code here.
 cnt = 0
-status = grid[0][0]
-for i in range(R-1):
-    for j in range(C-1):
-        if status != grid[i+1][j+1]:
-            status = grid[i+1][j+1]
-            cnt += 1
-            break
 
-print(cnt-1)
+for i in range(1,R):
+    for j in range(1,C):
+        # R-1 랑 C-1로 두는 건 열의 맨마지막으로 가면, 다음 점프 못함
+        for k in range(i+1, R-1):
+            for l in range(j+1, C-1):
+# \는 파이썬에서 여러 줄로 나누어서 적지만 한 줄로 읽히고 싶을 때 씀
+                if (grid[0][0] != grid[i][j] and 
+                grid [i][j] != grid[k][l] and 
+                # 맨마지막 행,열과 비교
+                grid[k][l] != grid[R-1][C-1] ):
+                    cnt += 1
+       
+
+print(cnt)
             
         
         
