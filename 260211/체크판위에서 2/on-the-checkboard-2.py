063bebef488551f8e00c47c4994a53454a490f92
Kimheekyo <khkyo010305@gmail.com>
2026-02-11 22:41:06 +0900

    Title: 체크판위에서 2, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260211/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,14 @@
+R, C = list(map(int,input().split()))
+arr = [list(map(str,input().split())) for _ in range(C)]
+
+cnt = 0
+
+for i in range(R):
+    for j in range(C):
+        for k in range(i+1,R-1):
+            for t in range(j+1,C-1):
+                if (arr[0][0] != arr[i][j]
+                and arr[i][j] != arr[k][t]
+                and arr[k][t] != arr[R-1][C-1]):
+                    cnt += 1
+print(cnt)
