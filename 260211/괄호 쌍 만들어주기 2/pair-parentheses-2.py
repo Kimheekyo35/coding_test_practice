1ae9cd306d80b4f279a76d81b25a1b9bcbd0b2e6
Kimheekyo <khkyo010305@gmail.com>
2026-02-11 22:57:14 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260211/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 260211/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -5,7 +5,7 @@ A = input()
 lena = len(A)
 cnt = 0
 for i in range(lena-2):
-    for j in range(2,lena-1):
+    for j in range(i+2,lena-1):
         if (A[i] =="(" and A[i+1]=="(") and (A[j]==")" and A[j+1]==")"):
             cnt += 1
 
